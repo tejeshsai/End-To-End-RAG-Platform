7a070fc353ac73297d2755658e38d5c571856697
Tejesh Sai Porandla <tejeshsaip@gmail.com>
2025-08-19 18:50:20 +0530

    Restructured Project Folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from pdfReader import PdfReader
 
 if __name__ == "__main__":
-    with PdfReader("sample-local-pdf.pdf") as r:
+    with PdfReader("data/sample-local-pdf.pdf") as r:
         print(r.get_page(0))
         print("Length of the pdf is ",r.get_page_count())
-        r.save_json("sample.json")
+        r.save_json("data/sample.json")
 
