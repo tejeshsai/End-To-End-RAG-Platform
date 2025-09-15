9bc8ccf416602ec9c7f05c4d63f9530eca909100
Tejesh Sai Porandla <tejeshsaip@gmail.com>
2025-09-15 22:55:22 +0530

    Update vector_store.py

--- src/embeddings/vector_store.py
+++ src/embeddings/vector_store.py
@@ -1,9 +1,12 @@
 from chromadb import PersistentClient
-
+import os
 class VectorStore:
     def __init__(self, name : str):
+        db_path = "chroma_db"
+        os.makedirs(db_path, exist_ok = True)
         self.client = PersistentClient(path = f"chroma_db")
         self.collection = self.client.get_or_create_collection(name = name)
+        self.name = name
 
     def add_documents(self, doc_id : str, text : str, embedding : list[float]):
         self.collection.add(
@@ -12,10 +15,10 @@ class VectorStore:
             embeddings = [embedding]
         )
 
-    def query(self, query_embedding : list[float], k : int = 3) -> list[str]:
+    def query(self, query_embedding : list[float], top_k : int = 3) -> list[str]:
         results = self.collection.query(
             query_embeddings= [query_embedding],
-            n_results = k
+            n_results = top_k
         )
         return results
 
